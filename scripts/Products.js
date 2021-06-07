19060eb4734730a5c04d290a5fc0750e1996ddab
hcrudge <hcrudge1@gmail.com>
2021-06-07 14:14:56 -0500

    Product Prices Chapter

--- scripts/Products.js
+++ scripts/Products.js
@@ -3,6 +3,35 @@ import { getProducts } from "./database.js"
 const products = getProducts()
 
 export const Products = () => {
+
+document.addEventListener(
+    "click",
+    (event) => {
+
+    if (event.target.id.startsWith("product")) {
+    // Start small. First, just log whether the click works or not
+    // console.log("hello, click!", event)
+
+   
+    // array destructuring 
+    const [, productId] = event.target.id.split("--") 
+
+    // productId changed to a number
+    const productIdNumber = parseInt(productId)
+
+    // find price for productId
+    let productPrice = 0
+
+    for (const product of products) {
+      if ( product.id === productIdNumber) {
+        productPrice = product.price
+      }
+    }
+    window.alert(`${event.target.innerText} costs $${productPrice}.`)
+  }
+}
+)
+
     let html = "<ul>"
 
     for (const product of products) {
